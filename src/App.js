f98243d21ba50901326a0d3570ee3fa72521da0b
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-21 15:07:56 -0500

    created an async function with try..catch script that fetch an array from an url, parsed it into a variable, and set the state value tour with that data

--- src/App.js
+++ src/App.js
@@ -5,7 +5,25 @@ const url = 'https://course-api.netlify.app/api/react-tours-project';
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [tours, setTours] = useState('');
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
 
   if (loading) {
     return (
@@ -24,8 +42,10 @@ function App() {
 
 export default App;
 
-// start by having two state values
-// loading default value is true meaning that we will see the Loading component
-// next, work on the return
-// in the condition, if loading is true it will return the jsx containing the Loading component
-// if false, it will run the default argument which is the Tours component
+// created a try..catch script using a fetch function that grabs an array of objects from an url and stores it into variable response
+// then parse that array and store it into tours variable
+// set state value loading to false
+// change the state value of tours with the json data
+// if an error is caught from the data, then set setLoading to false and console.log error
+// useEffect will run fetchTours function after render
+// what will be return onto the page is the Tour component since condition set is not true because the fetchTours sets setLoading to false when ran
