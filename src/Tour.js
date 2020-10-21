d21ac528766e56720ea5c9111ff470752cef78c5
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-21 16:48:52 -0500

    created an useState hook to toggle a button that will minimize and expand the text in the paragraph with a default boolean value of false; inside the <p> tag, created a ternary conditional that will showcase the full text or execute a substring function to minimize to a certain extent; created a button that toggles the state boolean value between truth and false that will control the expansion of the text

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 const Tour = ({ id, image, info, price, name }) => {
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
@@ -9,7 +11,12 @@ const Tour = ({ id, image, info, price, name }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
-        <p>{info}</p>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? 'show less' : 'read more'}
+          </button>
+        </p>
         <button className="delete-btn">not interested</button>
       </footer>
     </article>
@@ -20,3 +27,8 @@ export default Tour;
 
 // we'll destructured in the component's parameters with each prop that is listed in the array
 // structured your jsx to display your tour card
+// to create an interaction for your paragraph to minimize and expand the text, you will need to create an useState hook and a ternary operator within the html tag
+// the default state value will be a boolean value set as false
+// you'll create a ternary conditional in-between the <p> tags that has two arguments of either showing all of the text or a substring amount of the text (have to use template literal for substring function)
+// you'll create a button that takes in the state function setReadMore and toggles the boolean value between truth and false
+// create a button with another ternary conditional in-between its tag that has two arguments that toggles between 'show less' and 'read more' when clicked
