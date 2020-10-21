7d58063335319d7026bb240e66ca5efa202a6081
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-21 13:49:13 -0500

    created two useState hooks - loading (default boolean value) and tours (default value is an empty string); created a condition that if loading is true then return Loading component; if false, return Tours component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,28 @@ import Tours from './Tours';
 const url = 'https://course-api.netlify.app/api/react-tours-project';
 
 function App() {
-  return <h2>React Tour Project</h2>;
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState('');
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
 export default App;
+
+// start by having two state values
+// loading default value is true meaning that we will see the Loading component
+// next, work on the return
+// in the condition, if loading is true it will return the jsx containing the Loading component
+// if false, it will run the default argument which is the Tours component
