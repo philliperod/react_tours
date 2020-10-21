cd2ba7e92a471e55e6e00696b587e4c3f939a486
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-21 15:59:52 -0500

    in Tours component: set the parameter with the state value tours which contains the array from the url; structured layout of jsx; inside a div, use map() method to iterate the array and use the spread operator to expand its content in the Tour component; in the Tour component, structured your layout

--- src/Tours.js
+++ src/Tours.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+
+const Tours = ({ tours }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>our tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
+
+// now that you have obtained the data from the url and parsed it into json, you'll take that state value and use the map() method to iterate over the array
+// in the map() method, you'll return the Tour component which you set the key attribute to the id in the array and use the spread operator to expand it
